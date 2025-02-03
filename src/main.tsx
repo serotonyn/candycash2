5bbd300d9902559e63103ba219e4252c707c4b1c
serotonyn <youyou1youcef@gmail.com>
2025-02-03 15:44:56 +0100

    dayjs.locale("fr");

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import "@emotion/react";
 import "./index.css";
+import "dayjs/locale/fr";
 
+import dayjs from "dayjs";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import { AppTheme } from "./constants/theme.ts";
 import { AppThemeProvider } from "./providers/ThemeProvider.tsx";
+
+dayjs.locale("fr");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
